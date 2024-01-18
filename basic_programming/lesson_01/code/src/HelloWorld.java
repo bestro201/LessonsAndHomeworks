343ff88fe0fe30da82f0a9eb1322773f286c67dc
Bugaienko <sergiibugaienko@gmail.com>
2024-01-18 16:08:02 +0100

    Hello World

---
+++ basic_programming/lesson_01/code/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+@date 18.01.2024
+@author Sergey Bugaienko
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
