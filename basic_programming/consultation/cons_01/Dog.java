6f0f2d813ad36cff7447d0e7e97fee2d6458f8dd
StanislavPB <perminovsb@gmail.com>
2024-01-23 15:49:29 +0000

    add consultation 1

---
+++ basic_programming/consultation/cons_01/Dog.java
@@ -0,0 +1,14 @@
+public class Dog {
+
+    String name;
+
+    int age;
+
+    boolean isHangry;
+
+
+    public void bark(){
+        System.out.println("Моя собака громко лает!");
+    }
+
+}
---
+++ basic_programming/consultation/cons_01/PrimitiveTypes.java
@@ -0,0 +1,27 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        // ПРИМИТИВНЫЕ ТИПЫ ДАННЫХ
+
+        // целые числа
+
+        byte var1;  // -128 : 127 - 1 byte
+        short var2; // 2 byte
+        int var3;   // 4 byte
+        long var4;  // 8 byte
+
+
+        // плавающая точна
+
+        float var5;   // 4 byte
+        double var6;  // 8 byte
+
+        // символьные
+
+        char var7;    // 2 byte
+
+        // логические
+
+        boolean var8;  // 1 byte
+
+    }
+}
