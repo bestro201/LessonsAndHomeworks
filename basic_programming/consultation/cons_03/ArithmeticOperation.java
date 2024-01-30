87fa8741e033a27baa9b04dd7b835a12213d88a6
StanislavPB <perminovsb@gmail.com>
2024-01-30 15:54:21 +0000

    add consultation 3

---
+++ basic_programming/consultation/cons_03/ArithmeticOperation.java
@@ -0,0 +1,23 @@
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+
+       int x = 10;
+       int y = 3;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        double xDouble = 15.0;
+        double yDouble = 4.0;
+
+        System.out.println(xDouble / yDouble);
+        System.out.println(xDouble % yDouble);
+        // остаток от деления 15 / 4 =>
+        // целочисленный остаток от деления = 3
+        // остаток = 15 - 4 * 3 = 3
+
+
+    }
+}
