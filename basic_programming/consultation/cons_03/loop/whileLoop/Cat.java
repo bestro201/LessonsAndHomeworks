31ad788cce0899fb342ce7b54d40d3e3122ef13b
StanislavPB <perminovsb@gmail.com>
2024-01-30 15:57:42 +0000

    add consultation 3

---
+++ basic_programming/consultation/cons_03/loop/whileLoop/Cat.java
@@ -0,0 +1,19 @@
+package loop.whileLoop;
+
+public class Cat {
+    private String name;
+    private String breed;
+
+    public Cat(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+}
