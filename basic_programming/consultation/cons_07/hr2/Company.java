544cd9f4cbfef0d3c2baf586c7cd2ff63a29ca92
StanislavPB <perminovsb@gmail.com>
2024-02-13 16:03:48 +0000

    add consultation 7

--- basic_programming/consultation/cons_07/hr2/Company.java
+++ basic_programming/consultation/cons_07/hr2/Company.java
@@ -26,10 +26,11 @@ public class Company {
 
 // ??? как нам найти всех сотрудников из какого-то отдела ???
 
+        String searchDepartmentName = departments[3].getName();
+
         for (int i = 0; i < employees.length; i++) {
 
             String employeeDepartmentName = employees[i].getDepartment().getName();
-            String searchDepartmentName = departments[3].getName();
 
             if (employeeDepartmentName.equals(searchDepartmentName)) {
                 System.out.println(employees[i]);
