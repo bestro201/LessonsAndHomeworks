5a322a23f14a53fa488327396c1e09f27a4fd614
StanislavPB <perminovsb@gmail.com>
2024-02-20 15:57:31 +0000

    add consultation 9

---
+++ basic_programming/consultation/cons_09/generic4/MyMath.java
@@ -0,0 +1,17 @@
+package generic4;
+
+public class MyMath <T extends Number, S extends Number>{
+
+    T type1;
+
+    S type2;
+
+    public T getType1() {
+        return type1;
+    }
+
+    public S getType2() {
+        return type2;
+    }
+
+}
